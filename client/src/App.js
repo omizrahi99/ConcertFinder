9323067b891298d8497929940bdc5d3b1c8e08a0
Ori Mizrahi <omizrahi@ucsb.edu>
2020-08-04 12:57:34 -0700

    first commit

---
+++ client/src/App.js
@@ -0,0 +1,90 @@
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import SpotifyWebApi from "spotify-web-api-js";
+import axios from "axios";
+const spotifyApi = new SpotifyWebApi();
+
+class App extends Component {
+  constructor() {
+    super();
+    const params = this.getHashParams();
+    const token = params.access_token;
+    if (token) {
+      spotifyApi.setAccessToken(token);
+    }
+    this.state = {
+      loggedIn: token ? true : false,
+      nowPlaying: { name: "Not Checked", albumArt: "" },
+      myToken: token,
+    };
+  }
+
+  getHashParams() {
+    var hashParams = {};
+    var e,
+      r = /([^&;=]+)=?([^&;]*)/g,
+      q = window.location.hash.substring(1);
+    e = r.exec(q);
+    while (e) {
+      hashParams[e[1]] = decodeURIComponent(e[2]);
+      e = r.exec(q);
+    }
+    return hashParams;
+  }
+
+  getNowPlaying() {
+    spotifyApi.getMyCurrentPlaybackState().then((response) => {
+      this.setState({
+        nowPlaying: {
+          name: response.item.name,
+          albumArt: response.item.album.images[0].url,
+        },
+      });
+    });
+  }
+
+  async getLikedSongs() {
+    const response = await axios.get("https://api.spotify.com/v1/me/tracks", {
+      headers: {
+        Authorization: `Bearer ${this.state.myToken}`,
+      },
+    });
+    console.log(response.data);
+  }
+
+  async getTopArtists() {
+    const response = await axios.get(
+      "https://api.spotify.com/v1/me/top/artists",
+      {
+        headers: {
+          Authorization: `Bearer ${this.state.myToken}`,
+        },
+        params: {
+          limit: 50,
+        },
+      }
+    );
+    console.log(response.data);
+  }
+
+  render() {
+    this.getTopArtists();
+    return (
+      <div className='App'>
+        <a href='http://localhost:8888'> Login to Spotify </a>
+        <div>Now Playing: {this.state.nowPlaying.name}</div>
+        <div>
+          <img src={this.state.nowPlaying.albumArt} style={{ height: 150 }} />
+        </div>
+        {this.state.loggedIn && (
+          <button onClick={() => this.getNowPlaying()}>
+            Check Now Playing
+          </button>
+        )}
+      </div>
+    );
+  }
+}
+
+export default App;
